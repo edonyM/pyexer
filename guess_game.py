82e181dd4721132dc375c1f84ca42ec02ef47068
edonyM <edonyzpc@gmail.com>
2014-12-18 00:42:12 +0800

    add guess game

---
+++ guess_game.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#encoding utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2014-12-17 23:25
+ # 
+ # Filename: guess_game.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    '''
+    WARNING = '\033[0;32;43m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import random
+import string
+def gner():
+    num = raw_input("Enter: ")
+    return num
+
+#quiter = num
+#print 'a is %d' % a
+#print cmp(num,'exti')
+a = random.randint(1,9)
+while True:
+    num = gner()
+    if cmp(str(num),"exit") == 0:
+        break
+    else:
+        tmp = int(num)
+        if tmp > a:
+            print "Your number %s is larger than it!" % num
+            print "Enter it again!"
+            continue
+        elif tmp < a:
+            print "Your number %s is smaller than it!" % num
+            print "Enter it again!"
+            continue
+        else:
+            print "You got it!"
+            a = random.randint(1,9)
+#        quiter = num
