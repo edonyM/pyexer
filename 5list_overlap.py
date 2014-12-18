62dbed50bbca4ae32e82dcc2866b5dafdc0efcdc
edonyM <edonyzpc@gmail.com>
2014-12-18 15:47:26 +0800

    fix some little problem

---
+++ 5list_overlap.py
@@ -0,0 +1,74 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2014-12-17 14:19
+ # 
+ # Filename: 5list_overlap.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+tmp = []
+for ele in a:
+    for ele2 in b:
+        if ele == ele2:
+            if ele in tmp:#make sure the ele is not in the tmp
+                continue  #if yes, continue
+            else:
+                tmp.append(ele)
+        else:
+            continue
+
+print tmp
