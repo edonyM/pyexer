54d6cf27c9de79fc972e497e8c07f3f8c5f56c42
edonyM <edonyzpc@gmail.com>
2014-12-23 00:37:11 +0800

    add matplotlib with ml

---
+++ ml/fitlinear.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+#encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2014-12-23 00:26
+ # 
+ # Filename: fitlinear.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    '''
+    WARNING = '\033[0;32;43m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+from matplotlib import pyplot as mpl
+x = [1, 3, 5]
+t = [4.8, 11.3, 17.2]
+mpl.plot(x,t,'r+')
+mpl.axis([0,10,0,20])
+mpl.show()
---
+++ ml/fitlinear1.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+#encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2014-12-23 00:35
+ # 
+ # Filename: fitlinear1.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    '''
+    WARNING = '\033[0;32;43m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import matplotlib.pyplot as plt
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
