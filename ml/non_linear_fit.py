9f10a21a6e4f60f307c4c1cafb865869b23dfc7b
edonyM <edonyzpc@gmail.com>
2014-12-29 18:41:19 +0800

    3-degree fit

--- ml/non_linear_fit.py
+++ ml/non_linear_fit.py
@@ -39,13 +39,13 @@ import matplotlib.pyplot as plt
 x = np.array([1896,1900,1904,1906,1908,1912,1920,1924,1928,1932,1936,1948,1952,1956,1960,1964,1968    ,1972,1976,1980,1984,1988,1992,1996,2000,2004,2008])
 y = np.array([12.00,11.00,11.00,11.20,10.80,10.80,10.80,10.60,10.80,10.30,10.30,10.30,10.40,10.50,    10.20,10.00,9.95,10.14,10.06,10.25,9.99,9.92,9.96,9.84,9.87,9.85,9.69])
 one = np.ones(27)
-X = np.matrix([one,x,x**2]).getT()
+X = np.matrix([one,x,x**2,x**3]).getT()
 t = np.matrix(y).getT()
 X_Trans = X.getT()
 tmp = X_Trans * X
 tmp_inve = tmp.getI()
 out = tmp_inve * X_Trans * t
-func = lambda x,y:(x[0] + x[1] * y + x[2] * y**2)
+func = lambda x,y:(x[0] + x[1] * y + x[2] * y**2 + x[3] * y**3)
 res = np.array(out)
 x = np.array(X[:,1])
 y = np.array(y)
