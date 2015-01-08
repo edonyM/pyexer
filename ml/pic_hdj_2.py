12a3ad134a306e1e1540aa7f92c36ec2f1a27f95
edonyM <edonyzpc@gmail.com>
2015-01-08 22:46:11 +0800

    add out 2

---
+++ ml/pic_hdj_2.py
@@ -0,0 +1,87 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-01-08 22:03
+ # 
+ # Filename: pic_hdj_2.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import random as r
+
+z = np.array(np.linspace(0,100,num=200))
+y = np.array(np.ones(200)*10)
+theta = np.array(np.ones(200)*5)
+
+for i in range(200):
+    if (i+1)%2 == 0:
+        y[i] = -(y[i]-r.random()*0.7)
+        theta[i] = -(theta[i]+5+r.random()/5)
+
+for i in range(50,100):
+    theta[i] = 0.4*theta[i]
+    theta[i+50] = -0.5*theta[i]
+
+file = open('out2_mat.txt','w')
+for j in range(y.size):
+    file.write('%f %f %f'%(z[j],y[j],theta[j]))
+    file.write('\n')
+file.close()
+
+fig = plt.figure(1)
+ax = Axes3D(fig)
+ax.plot_wireframe(z,y,theta,color='r')
+plt.show()
