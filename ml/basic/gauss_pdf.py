b1ea9ef424b218885d557868104d4a4ee8c0df5c
edonyM <edonyzpc@gmail.com>
2015-01-14 19:07:55 +0800

    gauss probability density distribution

---
+++ ml/basic/gauss_pdf.py
@@ -0,0 +1,72 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-01-14 15:29
+ # 
+ # Filename: gauss_pdf.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as axes3d
+from scipy import stats as st
+
+y = np.array(range(11))
+p_gauss = st.gaussian_kde(y)
+
+plt.plot(y,np.zeros(y.size),'b^',markersize=10)
+x_eval = np.linspace(-10,10,num=200)
+plt.plot(x_eval,p_gauss(x_eval))
+plt.show()
---
+++ ml/basic/multivariate_gauss_pdf.py
@@ -0,0 +1,80 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-01-14 17:09
+ # 
+ # Filename: multivariate_gauss_pdf.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as axes3d
+from mpl_toolkits.mplot3d import Axes3D as axe
+from scipy import stats as st
+
+x1,x2 = np.mgrid[-2:4:0.01,-2:4:0.01]
+pos = np.empty(x1.shape+(2,))
+pos[:,:,0] = x1
+pos[:,:,1] = x2
+rv = st.multivariate_normal([2,1],[[1,0],[0,1]])
+p = rv.pdf(pos)
+rv2 = st.multivariate_normal([2,1],[[1,0.8],[0.8,1]])
+p2 = rv2.pdf(pos)
+fig = plt.figure(1)
+ax = axe(fig)
+ax.plot_surface(x1,x2,p,cstride=10,rstride=10,alpha=0.5,color='y')
+ax.plot_wireframe(x1,x2,p2,cstride=10,rstride=10,color='r')
+plt.show()
+
