b5dcf5a2599bc2d5c8606f2555f9caecae353a3c
edonyM <edonyzpc@gmail.com>
2015-01-30 22:52:18 +0800

    add normalize func

---
+++ ml/andrewNg/py/mlclass-ex1/featureNormalize.py
@@ -0,0 +1,106 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-01-30 22:23
+ # 
+ # Filename: featureNormalize.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import scipy as sp
+import math as m
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D as Ax3
+from scipy import stats as st
+from matplotlib import cm
+ 
+def featureNormalize(X):
+    #FEATURENORMALIZE Normalizes the features in X 
+    #   FEATURENORMALIZE(X) returns a normalized version of X where
+    #   the mean value of each feature is 0 and the standard deviation
+    #   is 1. This is often a good preprocessing step to do when
+    #   working with learning algorithms.
+
+    # You need to set these values correctly
+    X_norm = X;
+    mu = np.zeros((1,X.shape[1]));
+    sigma = np.zeros((1,X.shape[1]));
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: First, for each feature dimension, compute the mean
+    #               of the feature and subtract it from the dataset,
+    #               storing the mean value in mu. Next, compute the 
+    #               standard deviation of each feature and cdivide
+    #               each feature by it's standard deviation, storing
+    #               the standard deviation in sigma. 
+    #
+    #               Note that X is a matrix where each column is a 
+    #               feature and each row is an example. You need 
+    #               to perform the normalization separately for 
+    #               each feature. 
+    #
+    # Hint: You might find the 'mean' and 'std' functions useful.
+    #       
+    mu = np.mean(X,axis=0);
+    counter = X.shape[1];
+    for i in range(counter):
+        for j in range(X.shape[0]):
+            X_norm[j,i] = X[j,i] - mu[i];
+    sigma = np.std(X_norm);
+    for i in range(counter):
+        for j in range(X.shape[0]):
+            X_norm[j,i] = X_norm[j,i]/sigma;
+    # ============================================================
+    return [X_norm,mu,sigma]
