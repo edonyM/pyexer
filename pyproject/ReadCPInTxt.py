4f04652b28f24bd6180d5a74c98724e2000f0634
edonyM <edonyzpc@gmail.com>
2015-03-22 21:18:12 +0800

    add the script about read cloud points

---
+++ pyproject/ReadCPInTxt.py
@@ -0,0 +1,84 @@
+ #!/usr/bin/python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-03-22 16:38
+ # 
+ # Filename: ReadCPInTxt.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import scipy as sp
+import math as m
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D as Ax3
+from scipy import stats as st
+from matplotlib import cm
+ 
+def ReadCPInTxt(path):
+    file = open(path)
+    tmp = file.readline().split()
+    tmp = np.array(str2float(tmp))
+    tmp1 = file.readline().split()
+    tmp1 = np.array(str2float(tmp))
+    tmp = np.concatenate(([tmp],[tmp1]),axis=0)
+    for line in file.readlines():
+        line_tmp = np.array(str2float(line.split()))
+        tmp = np.concatenate((tmp,[line_tmp]),axis=0)
+    return tmp
+
+def str2float(x):
+    for i in range(len(x)):
+        x[i] = float(x[i])
+    return x
