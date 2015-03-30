deac5c88fc501a056cf0620d3babc091e531773f
edonyM <edonyzpc@gmail.com>
2015-03-30 12:39:48 +0800

    add sequence for kmeans

---
+++ pyproject/sequence.py
@@ -0,0 +1,101 @@
+ #!/usr/bin/env python
+ #encoding: utf-8
+ #        .---.         .-----------  
+ #       /     \  __  /    ------    
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_            
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->   
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    / 
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /) 
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`  
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'    
+ # ########################################################################################################
+ # 
+ # Author: edony - edonyzpc@gmail.com                 
+ # 
+ # twitter : @edonyzpc                                
+ # 
+ # Last modified: 2015-03-28 18:04
+ # 
+ # Filename: sequence.py
+ # 
+ # Description: All Rights Are Reserved                 
+class pcolor:
+    ''' This class is for colored print in the python interpreter!
+    "py" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor.
+    
+    格式: \033[显示方式;前景色;背景色m
+    说明:
+    前景色            背景色           颜色
+    ---------------------------------------
+    30                40              黑色
+    31                41              红色
+    32                42              绿色
+    33                43              黃色
+    34                44              蓝色
+    35                45              紫红色
+    36                46              青蓝色
+    37                47              白色
+    显示方式           意义
+    -------------------------
+    0                终端默认设置
+    1                高亮显示
+    4                使用下划线
+    5                闪烁
+    7                反白显示
+    8                不可见
+    
+    例子：
+    \033[1;31;40m   <!--1-高亮显示 31-前景色红色  40-背景色黑色-->
+    \033[0m         <!--采用终端默认设置，即取消颜色设置-->
+    '''
+    WARNING = '\033[0;37;41m'
+    ENDC = '\033[0m'
+    def disable(self):
+        self.ENDC = ''
+        self.WARNING = ''
+ 
+import numpy as np
+import scipy as sp
+import math as m
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D as Ax3
+from scipy import stats as st
+from matplotlib import cm
+ 
+
+def seq(fea_seq, X):
+    num_of_features = 0
+    fea = np.zeros(4)
+    for i in range(4):
+        if i in fea_seq:
+            num_of_features += 1
+            for j in fea_seq:
+                if j==i:
+                    fea[i] += 1
+
+    print num_of_features
+    #X = r.ReadCPInTxt('extraPoints.txt')
+    row = np.size(X,axis=0)
+    col = np.size(X,axis=1)
+    X_seq = np.zeros((row,col))
+    counter = 0
+    for j in range(4):
+        for i in range(65):
+            if fea_seq[i]==j:
+                X_seq[counter,:] = X[i,:]
+                counter += 1
+
+    print('writing file for sequence of classifier...\n......')
+    File = open('extraSeqPoints.txt','w')
+    for i in range(row):
+        File.write(X[i,0])
+        File.write(' ')
+        File.write(X[i,1])
+        File.write(' ')
+        File.write(X[i,2])
+        File.write('\n')
+    print('successful sequenced...')
