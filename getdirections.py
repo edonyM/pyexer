8259d5e852350e46c3e8c67bc03c00d2c56eb0fd
edonyM <edonyzpc@gmail.com>
2015-06-06 00:48:23 +0800

    add get directions recursively

---
+++ getdirections.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-06-05 15:48
+ #
+ # Filename: all_dirs.py
+ #
+ # Description: All Rights Are Reserved
+ #
+"""
+#import scipy as sp
+#import math as m
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D as Ax3
+#from scipy import stats as st
+#from matplotlib import cm
+#import numpy as np
+import os
+
+class PyColor(object):
+    """ This class is for colored print in the python interpreter!
+    "F3" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor."""
+    def __init__(self):
+        self.self_doc = r"""
+        STYLE: \033['display model';'foreground';'background'm
+        DETAILS:
+        FOREGROUND        BACKGOUND       COLOR
+        ---------------------------------------
+        30                40              black
+        31                41              red
+        32                42              green
+        33                43              yellow
+        34                44              blue
+        35                45              purple
+        36                46              cyan
+        37                47              white
+        DISPLAY MODEL    DETAILS
+        -------------------------
+        0                default
+        1                highlight
+        4                underline
+        5                flicker
+        7                reverse
+        8                non-visiable
+        e.g:
+        \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
+        \033[0m         <!--set all into default-->
+        """
+        self.warningcolor = '\033[0;37;41m'
+        self.tipcolor = '\033[0;31;42m'
+        self.endcolor = '\033[0m'
+        self._newcolor = ''
+    @property
+    def new(self):
+        """
+        Customized Python Print Color.
+        """
+        return self._newcolor
+    @new.setter
+    def new(self, color_str):
+        """
+        New Color.
+        """
+        self._newcolor = color_str
+    def disable(self):
+        """
+        Disable Color Print.
+        """
+        self.warningcolor = ''
+        self.endcolor = ''
+
+def find_subdir(path):
+    all_subdir = []
+    for item in os.listdir(path):
+        if os.path.isdir(item):
+            all_subdir.append(path + '/' + item)
+    return all_subdir
+
+def all_dir(path, total_dirs):
+    os.chdir(path)
+    tmp_dir = find_subdir(path)
+    total_dirs.append(tmp_dir)
+    if len(tmp_dir) == 0:
+        return
+    else:
+        for item in tmp_dir:
+            all_dir(item, total_dirs)
+
+def structed_dir(all_directions):
+    return sorted([direction for item in all_directions for direction in item])
+
+def getdir(path):
+    directions = []
+    all_dir(path, directions)
+    directions = structed_dir(directions)
+    return directions
+
+if __name__ == '__main__':
+#    total_dir = []
+#    all_dir('/home/edony/code/github/pyexer', total_dir)
+#    directions = structed_dir(total_dir)
+#    for direction in directions:
+#        print direction
+    path = '/home/edony/code/github/pyexer'
+    directions = getdir(path)
+    for direction in directions:
+        print direction
+
+
+
