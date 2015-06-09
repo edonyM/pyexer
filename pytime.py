2dec31061ba4788e3a29ffbff663040324ece0dc
edonyM <edonyzpc@gmail.com>
2015-06-09 16:02:10 +0800

    add python datetime module

---
+++ pytime.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-06-09 09:56
+ #
+ # Filename: pytime.py
+ #
+ # Description: All Rights Are Reserved
+ #
+"""
+#import scipy as sp
+#import math as m
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D as Ax3
+#from scipy import stats as st
+#from matplotlib import cm
+#import numpy as np
+import re
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
+
+class PyColor(object):
+    """ This class is for colored print in the python interpreter!
+    "F3" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor."""
+    def __init__(self):
+        self.self_doc = r"""
+        STYLE: \033['display model';'foreground';'background'm
+        DETAILS:
+        FOREGROUND        BACKGOUND       COLOR
+        ---------------------------------------
+        30                40              black
+        31                41              red
+        32                42              green
+        33                43              yellow
+        34                44              blue
+        35                45              purple
+        36                46              cyan
+        37                47              white
+        DISPLAY MODEL    DETAILS
+        -------------------------
+        0                default
+        1                highlight
+        4                underline
+        5                flicker
+        7                reverse
+        8                non-visiable
+        e.g:
+        \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
+        \033[0m         <!--set all into default-->
+        """
+        self.warningcolor = '\033[0;37;41m'
+        self.tipcolor = '\033[0;31;42m'
+        self.endcolor = '\033[0m'
+        self._newcolor = ''
+    @property
+    def new(self):
+        """
+        Customized Python Print Color.
+        """
+        return self._newcolor
+    @new.setter
+    def new(self, color_str):
+        """
+        New Color.
+        """
+        self._newcolor = color_str
+    def disable(self):
+        """
+        Disable Color Print.
+        """
+        self.warningcolor = ''
+        self.endcolor = ''
+
+def to_timestamp(dt_str, tz_str):
+    """
+    Translate the given time and timezone information into timestamp
+    """
+    tz_int = int(re.match('\w+([\-|\+][0-9]+)\:\d+', tz_str).group(1))
+    tz = timezone(timedelta(hours=tz_int))
+    dt = re.split('[\-\s\:]+', dt_str)
+    dt = [int(item) for item in dt]
+    in_time = datetime(dt[0], dt[1], dt[2], dt[3], dt[4], dt[5], tzinfo=tz)
+    #in_time.replace(tzinfo=tz)
+    return in_time.timestamp()
+
