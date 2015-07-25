43c22718dab00c3bcc9d7718705c1bef11ee15ce
edonyM <edonyzpc@gmail.com>
2015-07-25 11:00:27 +0800

    python decorator

--- decorator.py
+++ decorator.py
@@ -96,5 +96,24 @@ def dec(func):
 def dec_func():
     print 'edony'
 
+def deco(arg):
+    def _deco(func):
+        def __deco():
+            print("before %s called [%s]." % (func.__name__, arg))
+            func()
+            print("  after %s called [%s]." % (func.__name__, arg))
+        return __deco
+    return _deco
+
+@deco("mymodule")
+def myfunc():
+    print(" myfunc() called.")
+
+@deco("module2")
+def myfunc2():
+    print(" myfunc2() called.")
+
 if __name__ == '__main__':
     dec_func()
+    myfunc()
+    myfunc2()
