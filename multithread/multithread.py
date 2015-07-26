771083c540066b300a83a4fb4fe9a23b580ff87d
edonyM <edonyzpc@gmail.com>
2015-07-26 16:32:58 +0800

    multithread in python

---
+++ multithread/multithread.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-07-25 14:41
+ #
+ # Filename: multithread.py
+ #
+ # Description: All Rights Are Reserved
+ #
+"""
+#import scipy as sp
+#import math as m
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D as Ax3
+#from scipy import stats as st
+#from matplotlib import cm
+#import numpy as np
+from time import ctime
+from time import sleep
+import threading
+
+class PyColor(object):
+    """ This class is for colored print in the python interpreter!
+    "F3" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor."""
+    def __init__(self):
+        self.self_doc = r"""
+        STYLE: \033['display model';'foreground';'background'm
+        DETAILS:
+        FOREGROUND        BACKGOUND       COLOR
+        ---------------------------------------
+        30                40              black
+        31                41              red
+        32                42              green
+        33                43              yellow
+        34                44              blue
+        35                45              purple
+        36                46              cyan
+        37                47              white
+        DISPLAY MODEL    DETAILS
+        -------------------------
+        0                default
+        1                highlight
+        4                underline
+        5                flicker
+        7                reverse
+        8                non-visiable
+        e.g:
+        \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
+        \033[0m         <!--set all into default-->
+        """
+        self.warningcolor = '\033[0;31m'
+        self.tipcolor = '\033[0;32m'
+        self.endcolor = '\033[0m'
+        self._newcolor = ''
+    @property
+    def new(self):
+        """
+        Customized Python Print Color.
+        """
+        return self._newcolor
+    @new.setter
+    def new(self, color_str):
+        """
+        New Color.
+        """
+        self._newcolor = color_str
+    def disable(self):
+        """
+        Disable Color Print.
+        """
+        self.warningcolor = ''
+        self.endcolor = ''
+
+def activeone(func):
+    for i in range(2):
+        print("I am doing the active one. %s, %s"%(func, ctime()))
+        sleep(1)
+
+def activetwo(func):
+    for i in range(2):
+        print("I am doing the active two. %s, %s"%(func, ctime()))
+        sleep(5)
+
+def activethree(func):
+    for i in range(2):
+        print("I am doing the active one. %s, %s"%(func, ctime()))
+        sleep(4)
+
+def activefour(func):
+    for i in range(2):
+        print("I am doing the active two. %s, %s"%(func, ctime()))
+        sleep(4)
+threads = []
+t1 = threading.Thread(target=activeone, args=("music",))
+threads.append(t1)
+t2 = threading.Thread(target=activetwo, args=("movie",))
+threads.append(t2)
+t3 = threading.Thread(target=activethree, args=("BBox",))
+threads.append(t3)
+t4 = threading.Thread(target=activefour, args=("BBC",))
+threads.append(t4)
+if __name__ == "__main__":
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    # what about remove the line "t.join()"
+    t.join()
+    print("All done. %s"%ctime())
