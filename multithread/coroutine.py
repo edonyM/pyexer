573ebda99ba040aa6fd2485bcb7f4ef4cc1ad583
edonyM <edonyzpc@gmail.com>
2015-11-04 22:57:41 +0800

    add coroutin code

---
+++ multithread/coroutine.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-11-04 22:16
+ #
+ # Filename: coroutine.py
+ #
+ # Description: All Rights Are Reserved
+ #
+"""
+#import scipy as sp
+#import math as m
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D as Ax3
+#from scipy import stats as st
+#from matplotlib import cm
+#import numpy as np
+
+class PyColor(object):
+    """ This class is for colored print in the python interpreter!
+    "F3" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor."""
+    def __init__(self):
+        self.self_doc = r"""
+        STYLE: \033['display model';'foreground';'background'm
+        DETAILS:
+        FOREGROUND        BACKGOUND       COLOR
+        ---------------------------------------
+        30                40              black
+        31                41              red
+        32                42              green
+        33                43              yellow
+        34                44              blue
+        35                45              purple
+        36                46              cyan
+        37                47              white
+        DISPLAY MODEL    DETAILS
+        -------------------------
+        0                default
+        1                highlight
+        4                underline
+        5                flicker
+        7                reverse
+        8                non-visiable
+        e.g:
+        \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
+        \033[0m         <!--set all into default-->
+        """
+        self.warningcolor = '\033[0;31m'
+        self.tipcolor = '\033[0;32m'
+        self.endcolor = '\033[0m'
+        self._newcolor = ''
+    @property
+    def new(self):
+        """
+        Customized Python Print Color.
+        """
+        return self._newcolor
+    @new.setter
+    def new(self, color_str):
+        """
+        New Color.
+        """
+        self._newcolor = color_str
+    def disable(self):
+        """
+        Disable Color Print.
+        """
+        self.warningcolor = ''
+        self.endcolor = ''
+
+from threading import Thread
+from time import sleep
+from time import time
+
+def fib(n):
+    return fib(n-1) + fib(n-2) if n > 1 else n
+
+def timed_fib(n):
+    start_time = time()
+    fib(n)
+    end_time = time()
+    print("Executing fib took {} second.".format(end_time - start_time))
+    return int(end_time - start_time)
+
+def print_hello():
+    while True:
+        print("{} - Hello world!".format(int(time())))
+        sleep(1)
+
+def read_and_process_input():
+    while True:
+        n = int(input())
+        print('fib({}) = {}'.format(n, timed_fib(n)))
+
+def main():
+    # Second thread will print the hello message. Starting as a daemon means
+    # the thread will not prevent the process from exiting.
+    t = Thread(target=print_hello)
+    t.daemon = True
+    t.start()
+    # Main thread will read and process input
+    read_and_process_input()
+
+if __name__ == '__main__':
+    main()
